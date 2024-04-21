43200cbfa90838458fe7a0229806e3449ee479cb
Artem-Ky <artoad505@mail.ru>
2024-04-21 13:12:56 +0300

    feat: добавлен режим просмотра "лист"

--- components/Home/Filter/index.vue
+++ components/Home/Filter/index.vue
@@ -1,10 +1,29 @@
 <template>
-    <div class="filter">
-      <a href="#" class="filter__site filter__site-active">Все</a>
-      <a href="#" class="filter__site">Lenta.ru</a>
-      <a href="#" class="filter__site">Mos.ru</a>
+  <div class="filter">
+    <a href="#" class="filter__site filter__site-active">Все</a>
+    <a href="#" class="filter__site">Lenta.ru</a>
+    <a href="#" class="filter__site">Mos.ru</a>
 
-      <button class="filter__button filter__button-list"></button>
-      <button class="filter__button filter__button-grid filter__button-grid-active"></button>
-    </div>
+    <button @click="setListViewHandler" :class="`filter__button filter__button-list ${viewStateListActive}`"></button>
+    <button @click="setGridViewHandler" :class="`filter__button filter__button-grid ${viewStateGridActive}`"></button>
+  </div>
 </template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const viewState = computed(() => store.state.pageView.viewState);
+const viewStateGridActive = computed(() => viewState.value === 'grid' ? 'filter__button-grid-active' : '');
+const viewStateListActive = computed(() => viewState.value === 'grid' ? '' : 'filter__button-list-active');
+
+const setGridViewHandler = () => {
+store.dispatch('pageView/updateViewState', 'grid');
+};
+
+const setListViewHandler = () => {
+store.dispatch('pageView/updateViewState', 'list');
+}
+
+</script>
--- components/Home/NewsBlock/index.vue
+++ components/Home/NewsBlock/index.vue
@@ -1,28 +1,54 @@
 <template>
-    <div class="news">
-      <ul>
-        <li
-          v-for="newsItem in newsData"
-          :key="newsItem.link"
-          class="gridCard"
-        >
-          <h2 class="gridCard__title">{{ newsItem.title }}</h2>
-          <p v-if="newsItem.description" class="gridCard__description">
-            {{ newsItem.description }}
-          </p>
-          <a :href="newsItem.link" target="_blank" class="gridCard__link"
-            >Подробнее</a
-          >
-          <span class="gridCard__source">{{ newsItem.source }}</span>
-          <time class="gridCard__pubdate">{{ newsItem.pubDate }}</time>
-        </li>
-      </ul>
-    </div>
+  <div class="news">
+    <ul>
+      <li
+        v-for="newsItem in newsData"
+        :key="newsItem.link"
+        :class="viewStateContainer"
+      >
+      <a class="listCard__bigScreen" v-if="viewState === 'list'" :href="newsItem.link" target="_blank"></a>
+      <img v-if="viewState === 'list'" :class="viewStateImg" :src="newsItem.imageUrl" :alt="newsItem.title">
+        <h2 :class="viewStateTitle">{{ newsItem.title }}</h2>
+        <p v-if="newsItem.description" :class="viewStateDescription">
+          {{ newsItem.description }}
+        </p>
+        <a :href="newsItem.link" target="_blank" :class="viewStateLink">
+          Подробнее
+        </a>
+        <span :class="viewStateSource">{{ newsItem.source }}</span>
+        <time :class="viewStatePubDate">{{ newsItem.pubDate }}</time>
+      </li>
+    </ul>
+  </div>
 </template>
-  
-  <script setup>
-  import { useAsyncData } from 'nuxt/app';
-  import { fetchNews } from '~/utils/api';
-  const { data: newsData, error, refresh } = useAsyncData('news', fetchNews);
-  </script>
-  
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { useAsyncData } from 'nuxt/app';
+import { fetchNews } from '~/utils/api';
+
+const store = useStore();
+
+
+const { data: newsData, error, refresh } = useAsyncData('news', fetchNews);
+
+
+const viewState = computed(() => store.state.pageView.viewState);
+
+
+const viewStateContainer = computed(() => viewState.value === 'grid' ? 'gridCard' : 'listCard');
+const viewStateImg = computed(() => viewState.value === 'grid' ? '' : 'listCard__img');
+const viewStateTitle = computed(() => viewState.value === 'grid' ? 'gridCard__title' : 'listCard__title');
+const viewStateDescription = computed(() => viewState.value === 'grid' ? 'gridCard__description' : 'listCard__description');
+const viewStateLink = computed(() => viewState.value === 'grid' ? 'gridCard__link' : 'listCard__link');
+const viewStateSource = computed(() => viewState.value === 'grid' ? 'gridCard__source' : 'listCard__source');
+const viewStatePubDate = computed(() => viewState.value === 'grid' ? 'gridCard__pubdate' : 'listCard__pubdate');
+
+onMounted(() => {
+if (process.client) {
+  const storedViewState = localStorage.getItem('viewState') || 'grid';
+  store.dispatch('pageView/updateViewState', storedViewState);
+}
+});
+</script>
